1f874508e26c27d5ed3e78e96fd250613a41c379
Max <max30012540@hotmail.com>
2019-12-22 18:53:41 +0700

    แก้ Timing

--- dht/DHT22_lib.c
+++ dht/DHT22_lib.c
@@ -164,13 +164,18 @@ uint8_t bitInx = 7;
   
 	// == DHT will keep the line low for 80 us and then high for 80us ====
 
-	uSec = getSignalLevel( 85, 0 );
+    // Wait Start
+	uSec = getSignalLevel( 20000, 1 );
+//	ESP_LOGI( TAG, "Response = %d", uSec );
+	if( uSec<0 ) return DHT_TIMEOUT_ERROR; 
+
+	uSec = getSignalLevel( 200, 0 );
 //	ESP_LOGI( TAG, "Response = %d", uSec );
 	if( uSec<0 ) return DHT_TIMEOUT_ERROR; 
 
 	// -- 80us up ------------------------
 
-	uSec = getSignalLevel( 85, 1 );
+	uSec = getSignalLevel( 200, 1 );
 //	ESP_LOGI( TAG, "Response = %d", uSec );
 	if( uSec<0 ) return DHT_TIMEOUT_ERROR;
 
@@ -180,12 +185,12 @@ uint8_t bitInx = 7;
 
 		// -- starts new data transmission with >50us low signal
 
-		uSec = getSignalLevel( 56, 0 );
+		uSec = getSignalLevel( 100, 0 );
 		if( uSec<0 ) return DHT_TIMEOUT_ERROR;
 
 		// -- check to see if after >70us rx data is a 0 or a 1
 
-		uSec = getSignalLevel( 75, 1 );
+		uSec = getSignalLevel( 200, 1 );
 		if( uSec<0 ) return DHT_TIMEOUT_ERROR;
 
 		// add the current read to the output data
